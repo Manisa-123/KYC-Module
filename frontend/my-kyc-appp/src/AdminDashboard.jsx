13f33b5eb891539ef65a9d18a44cb232e02bcce2
Manisa-123 <37616247+Manisa-123@users.noreply.github.com>
2025-02-17 19:43:09 +0530

    Update AdminDashboard.jsx
    port changes

--- frontend/my-kyc-appp/src/AdminDashboard.jsx
+++ frontend/my-kyc-appp/src/AdminDashboard.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
-const API_URL = 'http://0.0.0.0:8000';
+const API_URL = 'http://13.200.68.58:8000';
 
 const AdminDashboard = () => {
     const [analytics, setAnalytics] = useState({});
